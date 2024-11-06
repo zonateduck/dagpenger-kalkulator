4428316e8f4f083a9c05de656a4e109bd37bb7da
zonateduck <sjokokatten@gmail.com>
2024-11-06 08:57:00 +0100

    La til variabler og funksjonalitet i resultat-klassen med kommentarer. Et resultat har nå en beregnet dagsats, spesialisering og godkjenning, som kan hentes ut.

--- src/main/java/no/nav/dagpenger/Resultat.java
+++ src/main/java/no/nav/dagpenger/Resultat.java
@@ -8,15 +8,60 @@ package no.nav.dagpenger;
  * 
  * Klassen inneholder informasjon om:
  * - Inntekten som ble brukt i beregningen.
- * - Status for resultatet (for eksempel innvilget, innvilget med makssats, eller avslag).
+ * - Status for resultatet (innvilget, innvilget med makssats, eller avslag).
  * - Om resultatet har blitt godkjent av en saksbehandler.
  *
  * @author Mila Toneff
  * @version 1.0
  */
 public class Resultat {
+    /* Beregnet dagsats hentet fra kalkulatoren */
+    private final double beregnetDagsats;
+    /* Spesialisering resultatet ligger innenfor */
+    private final Spesialisering spesialisering;
+    /* Hvorvidt resultatet har blitt godkjent av en saksbehandler */
+    private boolean erGodkjent;
 
-    public Resultat {
+    /**
+     * Oppretter et nytt resultat med beregnet dagsats og spesialisering.
+     * @param beregnetDagsats Dagsatsen som er beregnet.
+     * @param spesialisering Spesialiseringen for resultatet.
+     */
+    public Resultat(double beregnetDagsats, Spesialisering spesialisering) {
+        this.beregnetDagsats = beregnetDagsats;
+        this.spesialisering = spesialisering;
+        this.erGodkjent = spesialisering.erGodkjent();
     }
 
+    /**
+     * Henter den beregnede dagsatsen.
+     * @return Beregnet dagsats.
+     */
+    public double hentBeregnetDagsats() {
+        return beregnetDagsats;
+    }
+
+    /**
+     * Henter spesialiseringen knyttet til resultatet.
+     * @return Spesialiseringen.
+     */
+    public Spesialisering hentSpesialisering() {
+        return spesialisering;
+    }
+
+    /**
+     * Sjekker om resultatet er godkjent.
+     * @return True hvis resultatet er godkjent, ellers false.
+     */
+    public boolean erGodkjent() {
+        return erGodkjent;
+    }
+
+    /**
+     * Setter resultatet som godkjent eller avslått av saksbehandleren.
+     * @param erGodkjent Om resultatet er godkjent.
+     */
+    public void settGodkjenning(boolean erGodkjent) {
+        this.erGodkjent = erGodkjent;
+    }
 }
